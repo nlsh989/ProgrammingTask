3572c59b9026f860f2fa93d05a5d25d5f990bf87
Nilesh Kumar <nlsh989@gmail.com>
2018-04-27 00:11:11 +0530

    Initial commit

---
+++ assets/Scripts/UIManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject choicePrefab;
+	[SerializeField]
+	private GameObject refreshIcan;
+	[SerializeField]
+	private Text questionText;
+	[SerializeField]
+	private Transform choicesPanel;
+
+	private bool mIsRotating = false;
+
+	/// <summary>
+	/// Raises the clicked refresh button event.
+	/// </summary>
+	public void OnClickedRefreshBtn ()
+	{
+		if (!mIsRotating) {
+			//Hit the webservice here
+			WebserviceManager.Instance.GetDataFromServer (ServerResponseCallBack);
+			StartCoroutine (RotateCor ());
+		}
+	}
+
+	/// <summary>
+	/// Rotates the cor.
+	/// </summary>
+	/// <returns>The cor.</returns>
+	IEnumerator RotateCor ()
+	{
+		mIsRotating = true;
+		float speed = 1f;
+		while (mIsRotating) {
+			refreshIcan.transform.Rotate (Vector3.forward * speed);
+			yield return null;
+		}
+	}
+
+	/// <summary>
+	/// Servers the response call back.
+	/// </summary>
+	/// <param name="questionObject">Question object.</param>
+	void ServerResponseCallBack (QuestionEntity questionObject)
+	{
+		mIsRotating = false;
+		if (questionObject != null) {
+			StartCoroutine (SetUICor (questionObject));
+		}
+	}
+
+	/// <summary>
+	/// Sets the user interface cor.
+	/// </summary>
+	/// <returns>The user interface cor.</returns>
+	/// <param name="questionObject">Question object.</param>
+	IEnumerator SetUICor (QuestionEntity questionObject)
+	{
+		//clear old ui
+		for (int i = 0; i < choicesPanel.childCount; i++) {
+			Destroy (choicesPanel.GetChild (i).gameObject);
+		}
+
+		//wait until destroy old ui
+		while (choicesPanel.childCount > 0) {
+			yield return new WaitForEndOfFrame ();
+		}
+
+		//set new ui
+		questionText.text = questionObject.question;
+		foreach (var choiceObj in questionObject.choices) {
+			GameObject clone = Instantiate (choicePrefab, choicesPanel);
+			Text[] textArr = clone.GetComponentsInChildren<Text> ();
+			textArr [0].text = choiceObj.choice;
+			textArr [1].text = choiceObj.votes;
+		}
+	}
+
+}
---
+++ assets/Scripts/WebserviceManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+
+public delegate void ResponseCallBack (QuestionEntity questionObject);
+
+public class WebserviceManager : MonoBehaviour
+{
+	private readonly string QUESTIONS_URL = "https://private-5b1d8-sampleapi187.apiary-mock.com/questions";
+
+	private static WebserviceManager mInstance;
+
+	/// <summary>
+	/// Awake this instance.
+	/// </summary>
+	void Awake ()
+	{
+		DontDestroyOnLoad (this);
+	}
+
+	/// <summary>
+	/// Gets the instance.
+	/// </summary>
+	/// <value>The instance.</value>
+	public static WebserviceManager Instance {
+		get {
+			if (mInstance == null) {
+				mInstance = new GameObject ("WebserviceManager").AddComponent<WebserviceManager> ();
+			}
+			return mInstance;
+		}
+	}
+
+	/// <summary>
+	/// Gets the data from server.
+	/// </summary>
+	/// <param name="callback">Callback.</param>
+	public void GetDataFromServer (ResponseCallBack callback)
+	{
+		StartCoroutine (GetDataFromServerCor (callback));
+	}
+
+	/// <summary>
+	/// Gets the data from server cor.
+	/// </summary>
+	/// <returns>The data from server cor.</returns>
+	/// <param name="callback">Callback.</param>
+	private IEnumerator GetDataFromServerCor (ResponseCallBack callback)
+	{
+		WWW www = new WWW (QUESTIONS_URL);
+		yield return www;
+		QuestionEntity questionObject = null;
+		if (!string.IsNullOrEmpty (www.error)) {
+			print ("Error : " + www.error);
+		} else {
+			string serviceData = www.text;
+			string jsonStr = JSON.Parse (serviceData) [0].ToString ();
+			Debug.Log (jsonStr);
+			questionObject = JsonUtility.FromJson<QuestionEntity> (jsonStr);
+		}
+		callback (questionObject);
+	}
+}
+
+[System.Serializable]
+public class QuestionEntity
+{
+	public string question;
+	public string published_at;
+	public ChoiceEntity[] choices;
+}
+
+[System.Serializable]
+public class ChoiceEntity
+{
+	public string choice;
+	public string votes;
+}
